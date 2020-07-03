1dd9423fdf88b91fff66292fad2abe85b614415d
hajdumozes <hajdu.mozes@gmail.com>
2020-07-03 21:59:57 +0200

    fizzbuzz enum added #2

---
+++ src/main/java/com/example/fizzbuzz/config/FizzBuzz.java
@@ -0,0 +1,29 @@
+package com.example.fizzbuzz.config;
+
+import com.example.fizzbuzz.util.LeastCommonMultiple;
+import lombok.Getter;
+
+import java.math.BigInteger;
+
+// I wanted both the name and the values to come from a variable to avoid future headaches, when changes are needed.
+// Since I don't need to change the values right now, and there is no specification how should it be done in the future, I chose an enum for it.
+// I'd personally make the values settable on the front-end, and the enum fields could get a setter as well, so it wouldn't require a big refactoring.
+
+@Getter
+public enum FizzBuzz {
+    FIZZ("Fizz", 3),
+    BUZZ("Buzz", 5),
+    FIZZ_BUZZ(FIZZ.name.concat(BUZZ.name),
+            LeastCommonMultiple.getLcmForFizzBuzz(
+                    BigInteger.valueOf(FIZZ.value),
+                    BigInteger.valueOf(BUZZ.value)
+            ).intValue());
+
+    private String name;
+    private int value;
+
+    FizzBuzz(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+}
