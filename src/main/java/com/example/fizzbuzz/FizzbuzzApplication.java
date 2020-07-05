45e0d309822ff420c594bf291a7903c22eb1e4fd
hajdumozes <hajdu.mozes@gmail.com>
2020-07-05 17:15:46 +0200

    prepare for tomcat #4

--- src/main/java/com/example/fizzbuzz/FizzbuzzApplication.java
+++ src/main/java/com/example/fizzbuzz/FizzbuzzApplication.java
@@ -2,9 +2,10 @@ package com.example.fizzbuzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FizzbuzzApplication {
+public class FizzbuzzApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FizzbuzzApplication.class, args);
