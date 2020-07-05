749efeb2a3d22335a1f11fe113abebdc4c8e0f88
hajdumozes <hajdu.mozes@gmail.com>
2020-07-05 11:20:27 +0200

    clean code refactor #1

--- src/main/java/com/example/fizzbuzz/service/FizzBuzzServiceImpl.java
+++ src/main/java/com/example/fizzbuzz/service/FizzBuzzServiceImpl.java
@@ -12,25 +12,52 @@ public class FizzBuzzServiceImpl implements FizzBuzzService {
 
     @Override
     public List<String> getFizzBuzzSequence(int last) {
+        checkSequenceLength(last);
+        return getSequenceList(last);
+    }
+
+    private void checkSequenceLength(int last) {
         if (last < 1) {
             throw new FizzBuzzException("The sequence length must be a positive number");
         }
+    }
+
+    private List<String> getSequenceList(int last) {
         List<String> sequence = new ArrayList<>();
         for (int i = 1; i <= last; i++) {
             // I use String instead of StringBuilder, because I don't modify it in a loop, so memory is not an issue.
             // Hence the more readable wins for me, which is String.
-            String item = "";
-            if (i % FizzBuzz.FIZZ.getValue() == 0) {
-                item += FizzBuzz.FIZZ.getName();
-            }
-            if (i % FizzBuzz.BUZZ.getValue() == 0) {
-                item += (FizzBuzz.BUZZ.getName());
-            }
-            if (item.equals("")) {
-                item = String.valueOf(i);
-            }
-            sequence.add(item);
+            addItem(sequence, i);
         }
         return sequence;
+    }
+
+    private void addItem(List<String> sequence, int i) {
+        String item = "";
+        item = checkFizz(item, i);
+        item = checkBuzz(item, i);
+        item = getNormalString(item, i);
+        sequence.add(item);
+    }
+
+    private String getNormalString(String item, int i) {
+        if (item.equals("")) {
+            item = String.valueOf(i);
+        }
+        return item;
+    }
+
+    private String checkBuzz(String item, int i) {
+        if (i % FizzBuzz.BUZZ.getValue() == 0) {
+            item += (FizzBuzz.BUZZ.getName());
+        }
+        return item;
+    }
+
+    private String checkFizz(String item, int i) {
+        if (i % FizzBuzz.FIZZ.getValue() == 0) {
+            item += FizzBuzz.FIZZ.getName();
+        }
+        return item;
     }
 }
